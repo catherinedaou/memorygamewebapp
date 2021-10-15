0bd8238dbf22094b99e7ee037c95861ce2e206e2
Catherine Daou <catherine.daou@gmail.com>
2021-10-15 10:33:56 +0300

    Adding all files

---
+++ code.js
@@ -0,0 +1,55 @@
+onEvent("Start", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("image1.1", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image1.2", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image1.3", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image1.4", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("image2.1", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image2.2", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("image2.3", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image2.4", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image3.1", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image3.2", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("image3.3", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image3.4", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image4.1", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image4.2", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image4.3", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("image4.4", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("Start", "click", function( ) {
+  setScreen("screen2");
+});
+
